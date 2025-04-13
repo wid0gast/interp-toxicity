0ac040d3f36de28f0372b6bb18dbe37044f07cc2
wid0gast <mohammadshaz529@gmail.com>
2025-04-13 12:26:49 +0000

    bert processing

--- bert_classifier.py
+++ bert_classifier.py
@@ -42,20 +42,19 @@ import json
 # os.environ['CUDA_VISIBLE_DEVICES'] = "7"
 
 
-# dataset = load_dataset("csv", data_files={"train": 'jigsaw/train.csv', "test": 'jigsaw/test.csv'})
-dataset = load_dataset('csv', data_files={'test': 'toxigen_alice.csv'})
+dataset = load_dataset("csv", data_files={"train": 'jigsaw/train.csv', "test": 'jigsaw/test.csv'})
+# dataset = load_dataset('csv', data_files={'test': 'toxigen_alice.csv'})
 
-# Load GPT-2 tokenizer
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-tokenizer.pad_token = tokenizer.eos_token  # GPT-2 doesn’t have a padding token
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+print(tokenizer.pad_token)
 
 # Tokenization function
 def tokenize_data(example):
-    return tokenizer(example["generation"], padding="max_length", truncation=True, max_length=128, return_tensors="pt")
+    return tokenizer(example["comment_text"], padding="max_length", truncation=True, max_length=128, return_tensors="pt")
 
 # Apply tokenization
 dataset = dataset.map(tokenize_data, batched=True)
-dataset = dataset.rename_column("prompt_label", "labels")  # Rename for consistency
+dataset = dataset.rename_column("toxic", "labels")  # Rename for consistency
 dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
 
 
@@ -72,16 +71,15 @@ class JigsawDataset(Dataset):
 
 # Create PyTorch DataLoaders
 batch_size = 64
-# train_dataloader = DataLoader(JigsawDataset(dataset["train"]), batch_size=batch_size, shuffle=True)
+train_dataloader = DataLoader(JigsawDataset(dataset["train"]), batch_size=batch_size, shuffle=True)
 val_dataloader = DataLoader(JigsawDataset(dataset["test"]), batch_size=batch_size, shuffle=False)
 
 
 device = "cuda:1" if torch.cuda.is_available() else "cpu"
 
 
-# Load GPT-2 into transformer_lens
 
-class GPT2Classifier(nn.Module):
+class BERTClassifier(nn.Module):
     def __init__(self, transformer, num_classes=2):
         super().__init__()
         self.transformer = transformer
@@ -108,69 +106,69 @@ class GPT2Classifier(nn.Module):
 
 # Initialize model and optimizer
 num_classes = 2
-model = HookedTransformer.from_pretrained("gpt2", device=device)
-model.load_state_dict(torch.load("finetuned_gpt2/transformer.pth", map_location=device))
+model = HookedEncoder.from_pretrained("bert-base-uncased", device=device)
+# model.load_state_dict(torch.load("finetuned_gpt2/transformer.pth", map_location=device))
 model.to(device)
-classifier = GPT2Classifier(model, num_classes)
-classifier.load_state_dict(torch.load("finetuned_gpt2/classifier.pth", map_location=device))
+classifier = BERTClassifier(model, num_classes)
+# classifier.load_state_dict(torch.load("finetuned_gpt2/classifier.pth", map_location=device))
 classifier.to(device)
 
 criterion = nn.CrossEntropyLoss()
-# optimizer = optim.Adam(classifier.parameters(), lr=5e-5)
+optimizer = optim.Adam(classifier.parameters(), lr=5e-5)
 
-# # Training loop
-# num_epochs = 10
-# best_epoch = -1
-# best_loss = 100
+# Training loop
+num_epochs = 10
+best_epoch = -1
+best_loss = 100
 
-# for epoch in range(num_epochs):
-#     print(f"Epoch {epoch+1}")
-#     classifier.train()
-#     total_train_loss = 0
-#     total_val_loss = 0
-#     correct_preds = 0
+for epoch in range(num_epochs):
+    print(f"Epoch {epoch+1}")
+    classifier.train()
+    total_train_loss = 0
+    total_val_loss = 0
+    correct_preds = 0
 
-#     for batch in tqdm(train_dataloader):
-#         input_ids, labels = batch["input_ids"].to(device), batch["labels"].to(device)
+    for batch in tqdm(train_dataloader):
+        input_ids, labels = batch["input_ids"].to(device), batch["labels"].to(device)
 
-#         optimizer.zero_grad()
-#         logits = classifier(input_ids)
-#         loss = criterion(logits, labels)
-#         loss.backward()
-#         optimizer.step()
+        optimizer.zero_grad()
+        logits = classifier(input_ids)
+        loss = criterion(logits, labels)
+        loss.backward()
+        optimizer.step()
 
-#         total_train_loss += loss.item()
-#     classifier.eval()
-#     for batch in val_dataloader:
-#         input_ids, labels = batch["input_ids"].to(device), batch["labels"].to(device)
+        total_train_loss += loss.item()
+    classifier.eval()
+    for batch in val_dataloader:
+        input_ids, labels = batch["input_ids"].to(device), batch["labels"].to(device)
 
-#         logits = classifier(input_ids)
-#         loss = criterion(logits, labels)
+        logits = classifier(input_ids)
+        loss = criterion(logits, labels)
 
-#         total_val_loss += loss.item()
-#         correct_preds += sum(logits.argmax(dim=1) == labels).item()
+        total_val_loss += loss.item()
+        correct_preds += sum(logits.argmax(dim=1) == labels).item()
 
-#     avg_train_loss = total_train_loss / len(train_dataloader)
-#     avg_val_loss = total_val_loss / len(train_dataloader)
-#     print(f"Epoch {epoch+1}, Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss}")
-#     save_dir = "finetuned_gpt2"
-#     if avg_val_loss < best_loss:
-#         best_loss = avg_val_loss
-#         best_epoch = epoch
-#         torch.save(classifier.state_dict(), os.path.join(save_dir, f"classifier.pth"))
-#         torch.save(model.state_dict(), os.path.join(save_dir, f"transformer.pth"))
-
-
+    avg_train_loss = total_train_loss / len(train_dataloader)
+    avg_val_loss = total_val_loss / len(train_dataloader)
+    print(f"Epoch {epoch+1}, Train Loss: {avg_train_loss:.4f}, Val Loss: {avg_val_loss}")
+    save_dir = "finetuned_bert/jigsaw"
+    if avg_val_loss < best_loss:
+        best_loss = avg_val_loss
+        best_epoch = epoch
+        torch.save(classifier.state_dict(), os.path.join(save_dir, f"classifier.pth"))
+        torch.save(model.state_dict(), os.path.join(save_dir, f"transformer.pth"))
 
 
-# def get_log_probs(
-#     logits: Float[Tensor, "batch posn d_vocab"], tokens: Int[Tensor, "batch posn"]
-# ) -> Float[Tensor, "batch posn-1"]:
-#     logprobs = logits.log_softmax(dim=-1)
-#     # We want to get logprobs[b, s, tokens[b, s+1]], in eindex syntax this looks like:
-#     print(logits.shape, logprobs.shape, tokens.shape)
-#     correct_logprobs = eindex(logprobs, tokens, "b s [b s+1]")
-#     return correct_logprobs
+
+
+def get_log_probs(
+    logits: Float[Tensor, "batch posn d_vocab"], tokens: Int[Tensor, "batch posn"]
+) -> Float[Tensor, "batch posn-1"]:
+    logprobs = logits.log_softmax(dim=-1)
+    # We want to get logprobs[b, s, tokens[b, s+1]], in eindex syntax this looks like:
+    print(logits.shape, logprobs.shape, tokens.shape)
+    correct_logprobs = eindex(logprobs, tokens, "b s [b s+1]")
+    return correct_logprobs
 
 def head_zero_ablation_hook(
     z: Float[Tensor, "batch seq n_heads d_head"],
@@ -180,7 +178,7 @@ def head_zero_ablation_hook(
     z[:, :, head_index_to_ablate, :] = 0.0
 
 def get_ablation_scores(
-    classifier: GPT2Classifier,
+    classifier: BERTClassifier,
     tokens: Int[Tensor, "batch seq"],
     labels,
     ablation_function: Callable = head_zero_ablation_hook,
@@ -228,11 +226,11 @@ for i, batch in tqdm(enumerate(val_dataloader), total=len(val_dataloader)):
             ablated_preds[layer][head] += ablated_pred_list[layer][head].tolist()
     ablation_scores += tmp_ablation_scores
     if i % 16 == 0:
-        torch.save(ablation_scores / ((i+1) * batch_size), "ablation_scores_alice.pth")
-with open('ablated_preds_alice.json', 'w') as f:
+        torch.save(ablation_scores / ((i+1) * batch_size), "bert_ablation_scores_jigsaw.pth")
+with open('bert_ablated_preds_jigsaw.json', 'w') as f:
     json.dump(ablated_preds, f)
-with open('base_preds_alice.json', 'w') as f:
+with open('bert_base_preds_jigsaw.json', 'w') as f:
     json.dump(base_preds, f)
 
 ablation_scores /= len(val_dataloader)
-torch.save(ablation_scores, "ablation_scores_alice.pth")
+torch.save(ablation_scores, "bert_ablation_scores_jigsaw.pth")
