c3bb3c35c217c7d799bece9d9b72b46e13e91ad6
wid0gast <mohammadshaz529@gmail.com>
2025-06-08 09:26:49 +0000

    pgd experiments

--- bert_toxicity_attacks.py
+++ bert_toxicity_attacks.py
@@ -218,12 +218,12 @@ class PGDBERTAttack:
             sampler=sampler,
             num_workers=0  # Disable multiprocessing to avoid CUDA issues
         )
-        
+        print(len(dataloader))
         results = []
         total_processed = 0
         total_successes = 0
         
-        for batch_idx, (batch_texts, batch_labels) in enumerate(dataloader):
+        for batch_idx, (batch_texts, batch_labels) in tqdm(enumerate(dataloader), total=len(dataloader)):
             batch_results = []
             batch_successes = 0
             
